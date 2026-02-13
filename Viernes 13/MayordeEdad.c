9c5fb1612bdc6cd656021cacb03ee1e284f8f856
MateoLoyo <mateo.loyo7882@alumnos.udg.mx>
2026-02-13 08:51:15 -0600

    Create MayordeEdad.c

---
+++ Viernes 13/MayordeEdad.c
@@ -0,0 +1,15 @@
+int main()
+{
+    int Edad;
+    printf("Ingresa tu edad: ");
+    scanf("%d", &Edad);
+    if(Edad>=18)
+    {
+        printf("\nEres mayor de edad\n");
+    }
+    else
+    {
+        printf("\nEres menor de edad\n");
+    }
+    return 0;
+}
