a5d98b14787189ffa5e3217040e6754245114dc0
MateoLoyo <mateo.loyo7882@alumnos.udg.mx>
2026-03-06 07:56:03 -0600

    Add Arreglos.c with temperature address printing

---
+++ Viernes6Mar/Arreglos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float temp1, temp2;
+    float temp[24];
+    int dummy;
+    printf("la direccion de &temp1: %d\n", &temp1);
+    printf("la direccion de &temp2: %d\n", &temp2);
+    printf("la direccion de temp[24]: %d\n", temp);
+    printf("la direccion de &temp[24]: %d\n", &temp[24]);
+    printf("la direccion de &dummy: %d\n", &dummy);
+    return 0;
+}
