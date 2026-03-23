eb974ec7aa484409f4c950e4968cf86539daf0d1
MateoLoyo <mateo.loyo7882@alumnos.udg.mx>
2026-03-22 21:51:48 -0600

    Update DireccionesArreglos.c

--- Viernes20Mar/DireccionesArreglos.c
+++ Viernes20Mar/DireccionesArreglos.c
@@ -1,1 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char Nombre[4][20];
+    char ArregloChar[6] = {'M', 'A', 'T', 'E', 'O', '\0'};
+    int Numeros[5];
+    int i;
 
+    printf("\n==========================\n");
+    printf("Ingresa tu nombre completo:");
+    printf("\n==========================\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        printf("Ingresa tu %d Nombre: ", i+1);
+        scanf("%s", Nombre[i]);
+    }
+    for(i = 0; i < 2; i++)
+    {
+        printf("Ingresa tu %d Apellido: ", i+1);
+        scanf("%s", Nombre[i+2]);
+    }
+
+    printf("\n================================================\n");
+    printf("El Nombre ingresado fue: %s %s %s %s\n", Nombre[0],
+           Nombre[1], Nombre[2], Nombre[3]);
+    printf("\nLa direccione de memoria de %s fue: %d\n", Nombre[0], &Nombre[0]);
+    printf("\nLa direccione de memoria de %s fue: %d\n", Nombre[1], &Nombre[1]);
+    printf("\nLa direccione de memoria de %s fue: %d\n",Nombre[2], &Nombre[2]);
+    printf("\nLa direccione de memoria de %s fue: %d\n", Nombre[3], &Nombre[3]);
+
+    printf("\n\nDireccion de memoria Nombre completo: %d\n", Nombre);
+
+    printf("\n================================================\n");
+    printf("Direccion de memoria de las partes de un\narreglo de caracteres :\n");
+    printf("================================================\n");
+
+    printf("%c: %d\n", ArregloChar[0], &ArregloChar[0]);
+    printf("%c: %d\n", ArregloChar[1], &ArregloChar[1]);
+    printf("%c: %d\n", ArregloChar[2], &ArregloChar[2]);
+    printf("%c: %d\n", ArregloChar[3], &ArregloChar[3]);
+    printf("%c: %d\n", ArregloChar[4], &ArregloChar[4]);
+
+    printf("Direccion de %s es: %d\n", ArregloChar, ArregloChar);
+
+    printf("\n================================================\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("Ingresa el %d numero: ", i+1);
+        scanf(" %d", &Numeros[i]);
+    }
+
+    printf("\nLa direccione de memoria de %d fue: %d\n", Numeros[0], &Numeros[0]);
+    printf("\nLa direccione de memoria de %d fue: %d\n", Numeros[1], &Numeros[1]);
+    printf("\nLa direccione de memoria de %d fue: %d\n", Numeros[2], &Numeros[2]);
+    printf("\nLa direccione de memoria de %d fue: %d\n", Numeros[3], &Numeros[3]);
+    printf("\nLa direccione de memoria de %d fue: %d\n", Numeros[4], &Numeros[4]);
+
+    printf("\n================================================\n");
+    return 0;
+}
